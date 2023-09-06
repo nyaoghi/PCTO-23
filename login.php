00caac4c042679901e8dff98e99c5ea988bc4bd1
nyaoghi <144173871+nyaoghi@users.noreply.github.com>
2023-09-06 21:03:35 +0200

    login.php

--- login.php
+++ login.php
@@ -1,4 +1,3 @@
-
 <head>
   <title>Registrazione Utente</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -14,10 +13,8 @@
 				<input type=text name="emaillog" id="exampleFormControlInput1" placeholder="nome@esempio.com" class="form-control" id="validationCustom03" required />
 			</div>
 			<div class="col-md-4 my-3" >
-      <label for="inputPassword5" class="form-label">Indirizzo di residenza</label>
-            <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
-				<label for="validationCustom04" class="form-label">Password </label>
-				<input type=text name="passwordlog" class="form-control" id="validationCustom04" required />
+      <label for="inputPassword5" class="form-label">Password</label>
+            <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock" name="passwordlog">
 			</div>
 			<br><input class="btn btn-primary" type="submit" value="ACCEDI">
       <a href="https://localhost/php/form.php"><input class="btn btn-primary" type="button" value="REGISTRATI"></a>
