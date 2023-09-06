28ddccfcc4a4840a98c5160736ca12dd4ca95ddc
nyaoghi <144173871+nyaoghi@users.noreply.github.com>
2023-09-06 21:04:14 +0200

    nuovoordine.php

--- nuovoordine.php
+++ nuovoordine.php
@@ -1,9 +1,12 @@
-
 <head>
 
 </head>
 <body>
 <h1>hai effettuato un nuovo ordine</h1>
+<form method="get" action="controllo.php">
+<input type="hidden" name="iduser" value="<?=$_POST['iduser'];?>">
+<a href="https://localhost/php/ordini.php?iduser=<?= $iduser; ?>"><button type="submit" class="btn btn-danger">TORNA ALLA HOMEPAGE</button></a>
+</form>
 <?php
     $servername = "localhost";
     $username = "root";
@@ -15,7 +18,7 @@
         die("Connection failed: " . mysqli_connect_error());
     }
 
-    $sql = 'INSERT into ordine (Ordine) values ('.'\''.$_POST['ordine'].'\''.'where ID = '. $_GET['iduser'].')';
+    $sql = 'INSERT into ordine (Ordine, IDUtente) values ('.'\''.$_POST['ordine'].'\''.','.'\''.$_POST['iduser'].'\''.')';
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
       } 
